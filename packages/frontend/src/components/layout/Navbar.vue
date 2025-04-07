e5d760d515c7d9ac9f8987209192af4fa45d8da8
Eugene Trigubov <trigubov.eugene@gmail.com>
2025-04-07 20:52:29 +0200

    fix ui navbar styles

--- packages/frontend/src/components/layout/Navbar.vue
+++ packages/frontend/src/components/layout/Navbar.vue
@@ -3,65 +3,50 @@
     <div class="container mx-auto">
       <!-- Left section -->
       <div class="flex-1">
-        <router-link to="/" class="btn btn-ghost normal-case text-xl">
+        <router-link :to="isAuthenticated ? '/' : '/login'" class="btn btn-ghost normal-case text-xl">
           Indie Todo
         </router-link>
       </div>
       
       <!-- Right section -->
-      <div class="flex-none">
-        <ul class="menu menu-horizontal px-1">
-          <!-- Public navigation -->
+      <div class="flex grow justify-end px-2">
+        <div class="flex items-stretch">
           <template v-if="!isAuthenticated">
-            <li>
-              <router-link to="/login">Login</router-link>
-            </li>
-            <li>
-              <router-link to="/signup">Sign Up</router-link>
-            </li>
+            <router-link to="/login" class="btn btn-ghost">Login</router-link>
+            <router-link to="/signup" class="btn btn-ghost">Sign Up</router-link>
           </template>
-          
-          <!-- Authenticated navigation -->
+
           <template v-else>
-            <li>
-              <router-link to="/tasks">Tasks</router-link>
-            </li>
-            <li>
-              <router-link to="/billing">Billing</router-link>
-            </li>
-            <li>
-              <div class="dropdown dropdown-end">
-                <label tabindex="0" class="btn btn-ghost btn-circle avatar">
-                  <div class="w-10 rounded-full bg-primary text-primary-content grid place-items-center">
-                    <span>{{ getInitials() }}</span>
-                  </div>
-                </label>
-                <ul tabindex="0" class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
-                  <li>
-                    <router-link to="/profile" class="justify-between">
-                      Profile
-                    </router-link>
-                  </li>
-                  <li>
-                    <a @click.prevent="logout">Logout</a>
-                  </li>
-                </ul>
-              </div>
-            </li>
+            <router-link to="/tasks" class="btn btn-ghost">Tasks</router-link>
+            <router-link to="/billing" class="btn btn-ghost">Billing</router-link>
+            <div class="dropdown dropdown-end">
+              <label tabindex="0" class="btn btn-ghost btn-circle avatar flex items-center justify-center p-0">
+                <div class="w-10 h-10 rounded-full bg-primary text-primary-content">
+                  <span class="flex items-center justify-center h-full">{{ getInitials() }}</span>
+                </div>
+              </label>
+              <ul tabindex="0" class="p-2 shadow menu dropdown-content bg-base-100 rounded-box">
+                <li @click="closeDropdown">
+                  <router-link to="/profile" class="justify-between">
+                    Profile
+                  </router-link>
+                </li>
+                <li>
+                  <a @click.prevent="logout">Logout</a>
+                </li>
+              </ul>
+            </div>
           </template>
-          
-          <!-- Theme toggle -->
-          <li>
-            <button @click="toggleTheme" class="btn btn-ghost btn-circle">
-              <svg v-if="theme === 'dark'" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
-              </svg>
-              <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
-              </svg>
-            </button>
-          </li>
-        </ul>
+
+          <button @click="toggleTheme" class="btn btn-ghost btn-circle ml-2 my-auto">
+            <svg v-if="theme === 'dark'" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
+            </svg>
+            <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
+            </svg>
+          </button>
+        </div>
       </div>
     </div>
   </div>
@@ -93,6 +78,13 @@ const toggleTheme = () => {
 
 const logout = () => {
   authStore.logout();
+};
+
+const closeDropdown = () => {
+  const dropdownContent = document.activeElement as HTMLElement;
+  if (dropdownContent) {
+    dropdownContent.blur();
+  }
 };
 
 // Initialize theme from localStorage
